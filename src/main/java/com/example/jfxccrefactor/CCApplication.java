d46cab6665084f1de4cbc62a462e409bbd375b9d
mammothdude <jgodoy@lvschools.org>
2022-05-15 17:40:09 -0700

    Credit Card JavaFX Scene Builder Project v1 - can't load local image files... v2 - local image files work, but can't be put in a subdirectory

--- src/main/java/com/example/jfxccrefactor/CCApplication.java
+++ src/main/java/com/example/jfxccrefactor/CCApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class CCApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CCApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(CCApplication.class.getResource("cc-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Credit Card 2");
         stage.setScene(scene);
